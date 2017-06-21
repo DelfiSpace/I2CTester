a83061e8298ea711205547d564759187b8923ee9
ChiaJiunWei <chiajw90@hotmail.com>
2017-06-21 16:53:13 +0200

    Added fault handler

--- I2CTester.ino
+++ I2CTester.ino
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 2016 by Stefano Speretta <s.speretta@tudelft.nl>
- *
- * I2CTester: this is an application designed to test the 
- * functionality of the I2C Tester setup. The setup includes several 
- * devices on different boards to perform a long duration test on 
- * the hardware and software implementation. It is designed to use 
- * Energia (the Arduino port for MSP microcontrollers) on an MSP432.
- *
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License 
- * version 3, both as published by the Free Software Foundation.
- *
- */
- 
+   Copyright (c) 2016 by Stefano Speretta <s.speretta@tudelft.nl>
+
+   I2CTester: this is an application designed to test the
+   functionality of the I2C Tester setup. The setup includes several
+   devices on different boards to perform a long duration test on
+   the hardware and software implementation. It is designed to use
+   Energia (the Arduino port for MSP microcontrollers) on an MSP432.
+
+   This file is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License
+   version 3, both as published by the Free Software Foundation.
+
+*/
+
 #include <DWire.h>
 #include <DSerial.h>
 #include <I2CScanner.h>
@@ -31,24 +31,24 @@ bool enablePrint = true;
 bool verbose = true;
 
 #if INA_bus_one
-  INA226 ina_bus1(wire, 0x44);
+INA226 ina_bus1(wire, 0x44);
 #endif
 #if INA_bus_four
-  INA226 ina_bus4(wire, 0x40);
+INA226 ina_bus4(wire, 0x40);
 #endif
 #if INA_bus_six
-  INA226 ina_bus6(wire, 0x45);
+INA226 ina_bus6(wire, 0x45);
 #endif
 
 #if PCA9550_bus_one
-  PCA9550 blinker1(wire, 0x60);
+PCA9550 blinker1(wire, 0x60);
 #endif
 #if PCA9550_bus_six
-  PCA9550 blinker6(wire, 0x61);
+PCA9550 blinker6(wire, 0x61);
 #endif
 
 #if MAX1237_bus_four
-  MAX1237 adc(wire);
+MAX1237 adc(wire);
 #endif
 
 unsigned char dev[128];
@@ -65,21 +65,21 @@ void scanBus(bool printText)
   devicesCount = 0;
   // scan the bus and count the available devices
   if (printText)
-  { 
-     serial.println();
-     serial.println("Scanning bus... ");
+  {
+    serial.println();
+    serial.println("Scanning bus... ");
   }
-  
+
   I2CScanner::scan(wire, deviceFound);
-  
+
   if (printText)
-  {   
-    for(int i = 0; i < devicesCount; i++)
+  {
+    for (int i = 0; i < devicesCount; i++)
     {
       if (dev[i] != 0)
       {
         serial.print("-> Found Address 0x");
-        if (dev[i] < 15) 
+        if (dev[i] < 15)
         {
           serial.print('0');
         }
@@ -87,12 +87,12 @@ void scanBus(bool printText)
         serial.println();
       }
     }
-    
+
     if (devicesCount == 0)
     {
       serial.println("No device found");
     }
-    
+
     serial.println("Scan completed");
     serial.println();
   }
@@ -103,13 +103,13 @@ void setup()
   // Initialize I2C master
   wire.setFastMode();
   wire.begin();
-  
+
   // initialize debug UART
   serial.begin();
-  
+
   // ensure the serial port is initialized
   delay(20);
-  
+
   serial.println();
   serial.println("-----------------------------------------------------");
   serial.println("------------------    I2CTester    ------------------");
@@ -119,59 +119,80 @@ void setup()
 
 void loop()
 {
-  #if scan_buses
-    scanBus(enablePrint);
-  #endif
-  
-  #if INA_bus_one
-    signed short i1 = ina_bus1.getCurrent();
+#if scan_buses
+  scanBus(enablePrint);
+#endif
+
+#if INA_bus_one
+  signed short i1;
+  if (ina_bus1.getCurrent(i1) == 0)
+  {
     if (enablePrint)
     {
       if (verbose || (i1 > 1500) || (i1 < 2))
       {
-          serial.print("Current BUS 1: ");
-          serial.print(i1, DEC);
-          serial.println(" mA");
+        serial.print("Current BUS 1: ");
+        serial.print(i1, DEC);
+        serial.println(" mA");
       }
     }
-  #endif
-  
-  #if INA_bus_four
-    signed short i4 = ina_bus4.getCurrent();
+  }
+  else
+  {
+    serial.println("Fail");
+  }
+#endif
+
+#if INA_bus_four
+  signed short i4;
+  if (ina_bus4.getCurrent(i4) == 0)
+  {
     if (enablePrint)
     {
       if (verbose || (i4 > 1500) || (i4 < 2))
       {
-          serial.print("Current BUS 4: ");
-          serial.print(i4, DEC);
-          serial.println(" mA");
+        serial.print("Current BUS 4: ");
+        serial.print(i4, DEC);
+        serial.println(" mA");
       }
     }
-  #endif
-  
-  #if INA_bus_six
-    signed short i6 = ina_bus6.getCurrent();
+  }
+  else
+  {
+    serial.println("Fail");
+  }
+#endif
+
+#if INA_bus_six
+  signed short i6;
+  if (ina_bus6.getCurrent(i6) == 0)
+  {
     if (enablePrint)
     {
       if (verbose || (i6 > 1500) || (i6 < 2))
       {
-          serial.print("Current BUS 6: ");
-          serial.print(i6, DEC);
-          serial.println(" mA");
+        serial.print("Current BUS 6: ");
+        serial.print(i6, DEC);
+        serial.println(" mA");
       }
     }
-  #endif
-  
-  #if MAX1237_bus_four
-    unsigned short val = adc.readSingleChannel() >> 1;
-    if (enablePrint)
+  }
+  else
+  {
+    serial.println("Fail");
+  }
+#endif
+
+#if MAX1237_bus_four
+  unsigned short val = adc.readSingleChannel() >> 1;
+  if (enablePrint)
+  {
+    if (verbose || (val > 1700) || (val < 1200))
     {
-      if (verbose || (val > 1700) || (val < 1200))
-       {
-          serial.print("ADC BUS 4: ");
-          serial.print(val, DEC);
-          serial.println(" mV");
-       }
+      serial.print("ADC BUS 4: ");
+      serial.print(val, DEC);
+      serial.println(" mV");
     }
-  #endif
+  }
+#endif
 }
